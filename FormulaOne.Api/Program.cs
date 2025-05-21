3a9940eb09988961c67d5f4f662f8b2e5465f36c
Gio Khvedelidze <giorgi.khvedelidze.1@btu.edu.ge>
2025-05-22 01:03:34 +0400

    fix/typo-in-connection-string

--- FormulaOne.Api/Program.cs
+++ FormulaOne.Api/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(
-    builder.Configuration.GetConnectionString("DefaultConnetion")
+    builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
